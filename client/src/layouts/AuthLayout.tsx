8142ca536bed85bee6f7ccd10e9ea8090affb5b6
Yurii <peteretep191@gmail.com>
2025-10-27 15:28:22 +0200

    upadte

--- client/src/layouts/AuthLayout.tsx
+++ client/src/layouts/AuthLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 const AuthLayout: React.FC = () => {
 
   return (
-     <div className="min-h-screen flex items-center justify-center bg-foreground-100">
+     <div className="min-h-screen flex items-center justify-center w-full">
         <Outlet />
       </div>
   );
